da4563edbb406a8950c53a5fed0952cd371bde60
andreaecija <andreitaecija@gmail.com>
2026-03-19 15:44:41 +0100

    Trabajo de hoy: apartado Contact funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar/Navbar'
-import Home from './components/Home/Home'
-import About from './components/About/About'
-import Footer from './components/Footer/Footer'
-import Contact from './components/Contact/contact';
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
+import Contact from './components/Contact/contact'; 
+import './App.css';
 
 function App() {
   return (
     <>
       <Navbar />
-      
+
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<Contact />} /> {}
         </Routes>
       </main>
 
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
