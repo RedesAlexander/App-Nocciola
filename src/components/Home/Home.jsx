067eb87809bf480371878a1753ddf252ddbf1e71
Jesus-Raad <raadwebdeveloper@gmail.com>
2026-03-24 20:11:46 +0100

    imgs linkeables en home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,25 @@
-import './Home.css';
-
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
 function Home() {
+  const navigate = useNavigate();
   return (
     <section id="home" className="home-section">
       <div className="home-video-container">
         <div className="video-placeholder">
-          <p>VIDEO EN BUCLE</p>
+          <img 
+            src="/img/EstampadoLogoNocciola.jpg"
+            alt="Hero Nocciola"
+          />
+        </div>
+        <div className="images-placeholder">
+        <img onClick={() => navigate("/products")}
+            src="/img/Nocciola_Almond_pstachio.png"
+            alt="Hero Nocciola"
+          />
+           <img onClick={() => navigate("/products")}
+            src="/img/TurronCream6.png"
+            alt="Hero Nocciola"
+          />
         </div>
       </div>
     </section>
