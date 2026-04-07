a0749a72dcc58247e35d932444eef66b8c3266c5
Jesus-Raad <raadwebdeveloper@gmail.com>
2026-04-07 17:50:39 +0200

    correccgion nombre productos

--- src/components/Products/productsData.js
+++ src/components/Products/productsData.js
@@ -1,7 +1,7 @@
 const productsData = [
   {
     id: 1,
-    name: "Almond Cream 60% - 180 g",
+    name: "Almond Cream 60% - 180g",
     image: "/img/almond-cream-60-180g.png",
     story:
       "Creamy almond spread, handcrafted in Campo Calabro, Italy. Made with 60% roasted Italian almonds, sugar, extra virgin olive oil, and salt. Provides 39 g of protein.",
@@ -11,7 +11,7 @@ const productsData = [
   },
   {
     id: 2,
-    name: "Pistachio Cream 60% - 180 g",
+    name: "Pistachio Cream 60% - 180g",
     image: "/img/pistachio-cream-60-180g.png",
     story:
       "Artisan-crafted in Calabria, Italy. Made with 60% roasted pistachios. Natural and nutritious. Provides 36 g of protein.",
@@ -22,7 +22,7 @@ const productsData = [
   },
   {
     id: 3,
-    name: "Almond Cream 66% - 200 g",
+    name: "Almond Cream 66% - 200g",
     image: "/img/almond-cream-66-200g.png",
     story:
       "Smooth and creamy almond spread, handcrafted in Jijona, Spain. Made with 66% Spanish almonds and honey. Provides 39 g of protein.",
@@ -34,7 +34,7 @@ const productsData = [
   },
   {
     id: 4,
-    name: "Pistachio Cream 55% - 200 g",
+    name: "Pistachio Cream 55% - 200g",
     image: "/img/pistachio-cream-55-200g.png",
     story:
       "Artisan pistachio spread crafted in Valladolid, Spain. Contains 55% roasted Spanish pistachios. Natural and nutritious. Provides 31 g of protein.",
